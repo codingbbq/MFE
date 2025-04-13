14e94f8446b72c528048df4c88abd2e7499e09c0
codingbbq <codingbbq@gmail.com>
2025-04-13 14:09:18 +0530

    Marketing Application setup

---
+++ packages/marketing/config/webpack.dev.js
@@ -0,0 +1,19 @@
+const { merge } = require('webpack-merge');
+const HTMLWebpackPlugin = require('html-webpack-plugin');
+const commonConfig = require('./webpack.common');
+
+const devConfig = {
+    mode: 'development',
+    devServer: {
+        historyApiFallback: {
+            index: 'index.html'
+        }
+    },
+    plugins: [
+        new HTMLWebpackPlugin({
+            template: './public/index.html'
+        })
+    ]
+}
+
+module.exports = merge(commonConfig, devConfig);
