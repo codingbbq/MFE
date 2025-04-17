1979132a08dc2c6dae03710231edf5b3b265c21d
codingbbq <codingbbq@gmail.com>
2025-04-17 20:26:53 +0530

    Debugging

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -4,7 +4,7 @@ const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
 const domain = process.env.PRODUCTION_DOMAIN;
-
+console.log('PRODUCTION_DOMAIN:', process.env.PRODUCTION_DOMAIN);
 const prodConfig = {
     mode: 'production',
     output: {
