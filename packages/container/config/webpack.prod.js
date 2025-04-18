b2511cc05f0889197bdee24488bc3c065caa8db7
codingbbq <codingbbq@gmail.com>
2025-04-18 13:24:15 +0530

    Test Deploy

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,8 +3,8 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = process.env.PRODUCTION_DOMAIN;
-console.log('PRODUCTION_DOMAIN:', process.env.PRODUCTION_DOMAIN);
+const domain = 'https://dfams7pzus8od.cloudfront.net';
+
 const prodConfig = {
     mode: 'production',
     output: {
