8baa77d07c3b81f1cb301ea116401814415361ac
codingbbq <codingbbq@gmail.com>
2025-04-18 14:15:38 +0530

    Header component

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -3,7 +3,7 @@ const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPl
 const commonConfig = require('./webpack.common');
 const packageJson = require('../package.json');
 
-const domain = 'https://dfams7pzus8od.cloudfront.net';
+const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
     mode: 'production',
